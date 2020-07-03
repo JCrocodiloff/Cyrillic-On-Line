a375ec0dab3755e0bc4fae3e15f00fe9d85b142e
Teo Teef <teo_tf@live.com>
2020-07-03 21:44:50 +0200

    Github - first commit

---
+++ build/precache-manifest.1479aeabd3221d4b284a985f18c3b372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a02541a42bf1ae79d0a08a38e89acf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05524d89ca24809ab78a",
+    "url": "/static/js/2.6785a77b.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.6785a77b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f5759fc2a0b3c5e387",
+    "url": "/static/js/main.bbddb1c5.chunk.js"
+  },
+  {
+    "revision": "0c72ed3878478cc47e69",
+    "url": "/static/js/runtime-main.fdfd8209.js"
+  },
+  {
+    "revision": "8e1d3bd9ca4dd804e7e90b3c30b122c7",
+    "url": "/static/media/logoCnz.8e1d3bd9.png"
+  },
+  {
+    "revision": "aa7553e43eed282489bc633eb486e72c",
+    "url": "/static/media/pozadina.aa7553e4.png"
+  }
+]);
