f60f9199a819f970b47f92db9a28c863ab2cf39e
Teo Teef <teo_tf@live.com>
2020-07-05 11:42:37 +0200

    Sreduvanje na linkot, dodavanje atribut rel i element span

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return <div className="footer">
         <p>Позадина: Лаврентиевски Летопис</p>
-        <a href="http://expositions.nlr.ru/LaurentianCodex/eng/manuscript3.html" target="_blank">Извор🔗</a>
+        <a href="http://expositions.nlr.ru/LaurentianCodex/eng/manuscript3.html" target="_blank" rel="noopener noreferrer">Извор<span>🔗</span></a>
     </div>
 }
 
