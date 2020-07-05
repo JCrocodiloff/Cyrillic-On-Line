9e84ffea9f2e50a66084fa8ca0f99f7fa43ac3f4
Teo Teef <teo_tf@live.com>
2020-07-05 11:47:15 +0200

    Brisenje emoxhi

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return <div className="footer">
         <p>Позадина: Лаврентиевски Летопис</p>
-        <a href="http://expositions.nlr.ru/LaurentianCodex/eng/manuscript3.html" target="_blank" rel="noopener noreferrer">Извор<span role="img" aria-label>🔗</span></a>
+        <a href="http://expositions.nlr.ru/LaurentianCodex/eng/manuscript3.html" target="_blank" rel="noopener noreferrer">Извор</a>
     </div>
 }
 
